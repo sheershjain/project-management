e388314abb68ace3ab397dfb902bb7379c7ee832
arvind <apple@apples-MacBook-Pro.local>
2022-12-26 21:51:04 +0530

    designation table created

---
+++ models/designation.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, Sequelize) => {
+  class Designation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Designation.init({
+     designationCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isAlpha: true
+      },
+      designationTitle: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        isAlpha: true
+      }
+  }, {
+    sequelize,
+    paranoid: true,
+    tableName: "designation",
+    modelName: 'Designation',
+  });
+  return Designation;
+};
