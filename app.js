01dce1e6ad70cead641b16dc9183884424329cfd
arvind <apple@apples-MacBook-Pro.local>
2022-12-26 20:13:23 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+// const ReqResLoggerMiddleware = require("./middlewares/req-res-logger");
+const { commonErrorHandler } = require("./helper/errorHandler");
+
+const app = express();
+app.use(express.json());
+
+// Enable cors support to accept cross origin requests
+app.use(cors({ origin: '*', optionsSuccessStatus: 200 }));
+
+app.use('/health', (_req, res) => {
+  res.send({ message: 'Application runing successfully!' });
+});
+
+// REST API entry point
+routes.registerRoutes(app);
+
+// 404 Error Handling
+app.use((req, res) => {
+    const message = 'Invalid endpoint';
+    commonErrorHandler(req, res, message, 400);
+
+});
+
+module.exports = app;
